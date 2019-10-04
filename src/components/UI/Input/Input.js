c4b5042ac2bd0330c8e213b2678c5b6d9bf4fa5b
vanathab <Bharath.Vanathadupula@cdk.com>
2019-10-04 16:51:34 -0700

    changes commit 10.04.2019

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,10 +4,25 @@ import classes from '../../UI/Input/Input.module.css';
 const input = ( props ) => {
     let inputElement = null;
     
-    switch (props.inputtype) {
-        case('input'): inputElement=<input className={classes.Input} {...props}/>; break;
-        case('textarea'): inputElement= <textarea className={classes.Textarea} {...props}/>; break;
-        default: inputElement=<input className={classes.Input} {...props}/>;
+    switch (props.elementType) {
+        case('input'): inputElement=<input 
+        className={classes.Input} 
+        {...props.elementConfig} 
+        value={props.value}/>; break;
+        case('textarea'): inputElement= <textarea 
+        className={classes.Textarea} 
+        {...props.elementConfig} 
+        value={props.value}/>; break;
+        case('select'): 
+        inputElement= (
+        <select 
+        className={classes.Textarea} 
+        value={props.value}>
+          {props.elementConfig.options.map(option=>(
+              <option key={option.value} value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>); break;
+        default: inputElement=<input className={classes.Input} {...props.elementConfig} value={props.value}/>;
     }
 
     return(
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,12 +8,56 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component{
   state={
-      name:''
-      ,email:'',
-      address:{
-          street:'',
-          postalCode:''
-      },
+      orderForm:{
+            name:{
+                elementType: 'input',
+                elementConfig:{
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },           
+                    street: {
+                        elementType: 'input',
+                        elementConfig:{
+                            type: 'text',
+                            placeholder: 'street'
+                        },
+                        value: ''
+                    }, 
+                    zipcode:{
+                        elementType: 'input',
+                        elementConfig:{
+                            type: 'text',
+                            placeholder: 'Your Zipcode'
+                        },
+                        value: ''
+                    }, 
+                    country: {
+                        elementType: 'input',
+                        elementConfig:{
+                            type: 'text',
+                            placeholder: 'Your Country'
+                        },
+                        value: ''
+                    },           
+            email: {
+                elementType: 'input',
+                elementConfig:{
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },   
+        deliveryMethod:  {
+            elementType: 'select',
+            elementConfig:{
+                options: [{value: 'fastest', displayValue:'Fastest'},
+                {value: 'cheapest', displayValue:'Cheapest'} ]
+            },
+            value: ''
+        }   
+      },     
       loading:false
   };
 
@@ -22,17 +66,7 @@ class ContactData extends Component{
         this.setState({loading :true});
         const order={
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer:{
-                name:'Bharath Chandra',
-                address:{
-                        street: '123 Main st',
-                        zipcode:'97006',
-                        country:'USA'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price,           
         }
         Axios.post('/orders.json',order)
         .then(response=>{
@@ -43,11 +77,21 @@ class ContactData extends Component{
   }
 
   render(){
+      const formElementsArray=[];
+      for(let key in this.state.orderForm){
+          formElementsArray.push({
+              id:key,
+              config: this.state.orderForm[key]
+          });
+      }
       let form = (<form>
-        <Input inputtype="input" type="text" name="name" placeholder="Your name"></Input>
-        <Input  inputtype="input" type="text" name="email" placeholder="Your Email"></Input>
-        <Input  inputtype="input" type="text" name="street" placeholder="Your street"></Input>
-        <Input  inputtype="input" type="text" name="postal" placeholder="Your Postal code"></Input>
+        {formElementsArray.map(formElement=>(
+            <Input 
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}/>
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
     </form>);
       if(this.state.loading){
